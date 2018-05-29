27b1b7c7b2fd6dd3bff8a032ff21799593745f4b
Dr.Forkoff <DD@0chan.one>
2018-05-29 22:21:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <? 
-$isup = file_get_contents('http://isup.me/0chanru.net');
+$isup = file_get_contents('http://isup.me/0-chanru.net');
 $up = (strpos($isup, "looks down from here") == false);
 if($up) include('up.php');
 else include('down.php');
