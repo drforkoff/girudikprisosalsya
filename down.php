63a829c0761d0d13c9facbaa0afcbc803eb52efd
Dr.Forkoff <DD@0chan.one>
2018-05-29 22:20:40 +0700

    Update down.php
    параши заменены

--- down.php
+++ down.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Присосалик отсосался</title>
+	<title>Гирудик отсосался</title>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="assets/prisosalik.css">
 </head>
@@ -15,13 +15,13 @@
 				<img class="leech-status" src="assets/off.png"></img>
 			</td>
 			<td class="info">
-				<h1><a target="_blank" href="http://0chanru.net/b">Присосалик отсосался.</a></h1>
+				<h1><a target="_blank" href="http://0-chanru.net/b">Гирудик отсосался.</a></h1>
 				Попробуйте альтернативы:<br />
-				<a target="_blank" href="http://n0l.ch">n0l.ch</a> |
-				<a target="_blank" href="http://nullchan.net/b">nullchan.net</a> |
-				<a target="_blank" href="http://0chan.cf/b">0chan.cf</a> |
-				<a target="_blank" href="http://inach.org/b">inach.org</a> |
-				<a target="_blank" href="http://1chan.ru/news/all">1chan.ru</a>
+				<a target="_blank" href="https://0chan.pl">Пшекоговнульч</a> |
+				<a target="_blank" href="https://nullnyan.net/b">Нульсрань</a> |
+				<a target="_blank" href="https://0chan.xyz/b">Хуёвый говнульч</a> |
+				<a target="_blank" href="https://lolifox.org/">Лолифокс</a> |
+				<a target="_blank" href="https://1chan.ca/news/all">1chan.ca</a>
 			</td>
 		</tr>
 	</table>
